f4915d67c62b62d770a13e854ab9a56076932c0f
sarah <islam-hussein@outlook.com>
2024-08-02 02:57:20 +0300

    update cloudinary

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -25,7 +25,7 @@ export const asyncHandler = (fn) => {
 
 cloudinary.config({
   cloud_name: process.env.cloud_name,
-  api_key: process.env.pi_key,
+  api_key: process.env.api_key,
   api_secret: process.env.api_secret,
 });
 
